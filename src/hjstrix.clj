a7d25a3ed75e865ddf262be8fbadc1488c88358e
Justin Barton <bartonjustin@gmail.com>
2015-02-23 01:26:23 +1300

    timeout and block run

--- src/hjstrix.clj
+++ src/hjstrix.clj
@@ -1,5 +1,5 @@
 (ns hjstrix
-  (:require [clojure.core.async :refer [chan go-loop go <! >! timeout]]))
+  (:require [clojure.core.async :refer [chan go-loop go <! >! timeout alts! close!]]))
 
 (def commands (atom {}))
 
@@ -10,25 +10,30 @@
     (let [ch (chan poolsize)]
       (swap! commands assoc (str run) {:run run :fallback fallback :timeout timeout :chan ch :poolsize poolsize})
       (go-loop [] 
-               (let [args (<! ch)] 
+               (let [[outchan args] (<! ch)] 
                     (try 
-                      (apply run args)
-                      (catch Exception e (if fallback (apply fallback e args) (throw e))))
+                      (prn args)
+                      (>! outchan (or (apply run args) ""))
+                      (catch Exception e 
+                        (close! outchan) 
+                        (if fallback (apply fallback e args) (throw e))))
                     (println "go-loop" (str run) args)) 
                (recur)))))
 
 (defn run 
   [func & args]
   (if-let [cmd (get @commands (str func))]
-    (do (go 
-          (>! (:chan cmd) args)
-          ))
+      (let [ch (chan)]
+       (>!! (:chan cmd) [ch args])
+       (alt!! (timeout (:timeout cmd)) (if-let [fallback (:fallback cmd)] (apply fallback "TIMEOUT" args) (println "ERROR! timeout"))
+              ch ([result] result))
+          )
     (println "ERROR! NO COMMAND" (str func)))) 
 
 (comment
-    (defn myfun [a] (println "myfun" a))
+    (defn myfun [a] (println "myfun" a) (Thread/sleep 5000) (println "myfun done"))
     (defn myfall [err & args] (println "myfall" err args))
-    (command myfun myfall 0 3)
+    (command myfun myfall 3000 3)
     (run myfun "bla") 
     (run myfun "moo") 
   ) 
